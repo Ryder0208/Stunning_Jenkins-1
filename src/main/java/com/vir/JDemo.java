794f9588af69a3aca4672e80e0a9661b4aa91958
Ryder0208 <vitthalg123@gmail.com>
2022-07-22 18:12:02 +0530

    project added

---
+++ src/main/java/com/vir/JDemo.java
@@ -0,0 +1,18 @@
+package com.vir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JDemo {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JDemo.class, args);
+		System.out.println("***Welcome to Jtest Program..***");
+		System.out.println("***Welcome to Jtest Program..***");
+		System.out.println("***Welcome to Jtest Program..***");
+		System.out.println("***Welcome to Jtest Program..***");
+		System.out.println("***Welcome to Jtest Program..***");
+	}
+
+}
