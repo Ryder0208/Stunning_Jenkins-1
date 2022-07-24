edcc333f325c583716844160c7f9c92f7f8e61aa
Ryder0208 <vitthalg123@gmail.com>
2022-07-24 12:22:28 +0530

    file modified

---
+++ src/main/java/com/vir/controller/JDemo.java
@@ -0,0 +1,15 @@
+package com.vir.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JDemo {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JDemo.class, args);
+		System.out.println("***program executed..***");
+	
+	}
+
+}
--- src/main/java/com/vir/controller/ServletInitializer.java
+++ src/main/java/com/vir/controller/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.vir;
+package com.vir.controller;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
